9bad76049ed40031c729aecb901ab69b11beae8d
DinaStadnytskaya <darknaya@outlook.com>
2023-03-10 20:05:12 +0200

    test

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,9 +10,9 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
+    // config.resolve.modules.push(paths.src);
     // config.resolve.modules = [paths.src, 'node_modules'];
-    // config.resolve.modules.unshift(paths.src);
+    config.resolve.modules.unshift(paths.src);
     // config.resolve.modules = [path.resolve(__dirname, '../../src'), 'node_modules'];
     config.resolve.extensions.push('.ts', '.tsx');
 
