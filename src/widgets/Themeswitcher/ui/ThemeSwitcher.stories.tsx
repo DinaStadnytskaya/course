851e2ed291a310d03d05ef2ab79ba6ce01dd4e05
DinaStadnytskaya <darknaya@outlook.com>
2023-03-12 23:04:33 +0200

    12

--- src/widgets/Themeswitcher/ui/ThemeSwitcher.stories.tsx
+++ src/widgets/Themeswitcher/ui/ThemeSwitcher.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/Theme';
-import { ThemeSwitcher } from './ThemeSwitcher.tsx';
+import { ThemeSwitcher } from './ThemeSwitcher.js';
 
 export default {
     title: 'widget/ThemeSwitcher',
