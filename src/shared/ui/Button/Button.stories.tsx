97136e7ae91253b8e9554ffba8f9d47fd5fc8bb9
DinaStadnytskaya <darknaya@outlook.com>
2023-03-12 17:42:02 +0200

    2

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { ThemeDecorator } from 'shared/config/storybook/Themedecorator/ThemeDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/Theme';
 import { Button, SizeButton, ThemeButton } from './Button';
 
