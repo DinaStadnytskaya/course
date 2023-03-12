24ac7220ec5e23159b094fc8b686970e1e3d81ff
DinaStadnytskaya <darknaya@outlook.com>
2023-03-12 19:02:13 +0200

    5

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
